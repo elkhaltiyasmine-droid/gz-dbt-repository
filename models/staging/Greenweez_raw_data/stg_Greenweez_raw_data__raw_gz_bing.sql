6864182d3d42a8fafecab624c1c8f521d1833566
elkhaltiyasmine@gmail.com <elkhaltiyasmine@gmail.com>
2025-09-30 15:35:14 +0000

    changes

--- models/staging/Greenweez_raw_data/stg_Greenweez_raw_data__raw_gz_bing.sql
+++ models/staging/Greenweez_raw_data/stg_Greenweez_raw_data__raw_gz_bing.sql
@@ -11,9 +11,9 @@ renamed as (
     select
         date_date,
         paid_source,
+        campaign_key,
         campgn_name AS campaign_name ,
         CAST(ads_cost AS FLOAT64) AS ads_cost ,
-        ads_cost,
         impression,
         click
 
